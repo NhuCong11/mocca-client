5035eac05457606f20d23e46c5ef252fa72b6f9a
Công <yeusangtao96@gmail.com>
2025-02-08 09:46:54 +0700

    Refactor AppFooter component to use dynamic rendering for footer links and social icons

---
+++ src/components/AppFooter/constant.ts
@@ -0,0 +1,22 @@
+import { IconBrandFacebook, IconBrandGithub, IconBrandInstagram } from '@tabler/icons-react';
+
+export const footerLinks = [
+  [
+    { href: '/about', key: 'footer.title01' },
+    { href: '/#!', key: 'footer.title03' },
+  ],
+  [
+    { href: '/#!', key: 'footer.title04' },
+    { href: '/#!', key: 'footer.title05' },
+  ],
+  [
+    { href: '/#!', key: 'footer.title06' },
+    { href: '/#!', key: 'footer.title07' },
+  ],
+];
+
+export const socialLinks = [
+  { href: 'https://www.facebook.com/Nhu.Cong1123', icon: IconBrandFacebook },
+  { href: '#!', icon: IconBrandInstagram },
+  { href: '#!', icon: IconBrandGithub },
+];
--- src/components/AppFooter/index.tsx
+++ src/components/AppFooter/index.tsx
@@ -1,98 +1,63 @@
+import React from 'react';
 import Image from 'next/image';
 import clsx from 'clsx';
-
 import { useTranslations } from 'next-intl';
 import CustomLink from '@/share/CustomLink';
-
 import styles from './AppFooter.module.scss';
-import { IconBrandFacebook, IconBrandGithub, IconBrandInstagram } from '@tabler/icons-react';
+import { footerLinks, socialLinks } from './constant';
 
 function AppFooter() {
   const t = useTranslations();
 
   return (
     <div className={clsx(styles.footer)}>
-      <div className={'container gx-5'}>
-        <CustomLink href={'/'}>
+      <div className="container gx-5">
+        <CustomLink href="/">
           <Image
             width={230}
             height={71}
-            src={'/images/logo-vip1.png'}
+            src="/images/logo-vip1.png"
             priority
             alt="Mocca Cafe"
             className={clsx(styles.footer__logo)}
           />
         </CustomLink>
 
-        <div className={clsx('separate')} style={{ '--separate-bg': 'transparent' } as React.CSSProperties}></div>
+        <div className="separate" style={{ '--separate-bg': 'transparent' } as React.CSSProperties}></div>
 
-        <div className={'row'}>
-          <div className={'col col-xxl-3 col-xl-3 col-12'}>
-            <ul>
-              <li>
-                <CustomLink href={'/about'} className={clsx(styles.footer__item)}>
-                  {t('footer.title01')}
-                </CustomLink>
-              </li>
-              <li>
-                <CustomLink href={'/#!'} className={clsx(styles.footer__item)}>
-                  {t('footer.title03')}
-                </CustomLink>
-              </li>
-            </ul>
-          </div>
+        <div className="row">
+          {footerLinks.map((column, colIndex) => (
+            <div key={colIndex} className="col col-xxl-3 col-xl-3 col-12">
+              <ul>
+                {column.map(({ href, key }) => (
+                  <li key={key}>
+                    <CustomLink href={href} className={clsx(styles.footer__item)}>
+                      {t(key)}
+                    </CustomLink>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
 
-          <div className={'col col-xxl-3 col-xl-3 col-12'}>
-            <ul>
-              <li>
-                <CustomLink href={'/#!'} className={clsx(styles.footer__item)}>
-                  {t('footer.title04')}
-                </CustomLink>
-              </li>
-              <li>
-                <CustomLink href={'/#!'} className={clsx(styles.footer__item)}>
-                  {t('footer.title05')}
-                </CustomLink>
-              </li>
-            </ul>
-          </div>
-
-          <div className={'col col-xxl-3 col-xl-3 col-12'}>
-            <ul>
-              <li>
-                <CustomLink href={'/#!'} className={clsx(styles.footer__item)}>
-                  {t('footer.title06')}
-                </CustomLink>
-              </li>
-              <li>
-                <CustomLink href={'/#!'} className={clsx(styles.footer__item)}>
-                  {t('footer.title07')}
-                </CustomLink>
-              </li>
-            </ul>
-          </div>
-
-          <div className={'col col-xxl-3 col-xl-3 col-12'}>
+          <div className="col col-xxl-3 col-xl-3 col-12">
             <div className={clsx(styles.footer__socials)}>
-              <a
-                href="https://www.facebook.com/Nhu.Cong1123"
-                rel="noreferrer"
-                target="_blank"
-                className={clsx(styles['footer__social-link'])}
-              >
-                <IconBrandFacebook size={28} color="var(--white)" />
-              </a>
-              <a href="#!" rel="noreferrer" target="_blank" className={clsx(styles['footer__social-link'])}>
-                <IconBrandInstagram size={28} color="var(--white)" />
-              </a>
-              <a href="#!" rel="noreferrer" target="_blank" className={clsx(styles['footer__social-link'])}>
-                <IconBrandGithub size={28} color="var(--white)" />
-              </a>
+              {socialLinks.map(({ href, icon: Icon }, index) => (
+                <a
+                  key={index}
+                  href={href}
+                  rel="noreferrer"
+                  target="_blank"
+                  className={clsx(styles['footer__social-link'])}
+                >
+                  <Icon size={28} color="var(--white)" />
+                </a>
+              ))}
             </div>
           </div>
         </div>
 
-        <div className={clsx('separate')}></div>
+        <div className="separate"></div>
 
         <div className={clsx(styles.footer__bottom)}>
           <div className={clsx(styles['footer__bottom-group'])}>
@@ -109,17 +74,16 @@ function AppFooter() {
               <p className={clsx(styles['footer__bottom-text'])}>© 2024 Mocca Cafe</p>
             </div>
             <div className={clsx(styles['footer__bottom-wrapper'])}>
-              <p className={clsx(styles['footer__bottom-text'])}>
-                <a href="#!" className={clsx(styles['footer__bottom-link'])}>
-                  {t('footer.title07')}
-                </a>
-              </p>
-              <span className={clsx('dot-separate')}>•</span>
-              <p className={clsx(styles['footer__bottom-text'])}>
-                <a href="#!" className={clsx(styles['footer__bottom-link'])}>
-                  {t('footer.title09')}
-                </a>
-              </p>
+              {['footer.title07', 'footer.title09'].map((key, index) => (
+                <React.Fragment key={key}>
+                  {index > 0 && <span className="dot-separate">•</span>}
+                  <p className={clsx(styles['footer__bottom-text'])}>
+                    <a href="#!" className={clsx(styles['footer__bottom-link'])}>
+                      {t(key)}
+                    </a>
+                  </p>
+                </React.Fragment>
+              ))}
             </div>
           </div>
         </div>
